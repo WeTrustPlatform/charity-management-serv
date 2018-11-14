f6768dfa998a24daf7f3a697934333ddcc23e190
Hoang Nguyen <hoang@wetrust.io>
2018-11-14 12:28:59 -0800

    autoformat terraform

--- staging.tf
+++ staging.tf
@@ -6,14 +6,17 @@ variable "db_password" {}
 variable "region" {
   default = "us-west-2"
 }
+
 # This ami_id depends on region
 # Ubuntu 18.04 hvm:ebs-ssd
 variable "ami_id" {
   default = "ami-079b4e9085609225c"
 }
+
 variable "env" {
   default = "staging"
 }
+
 variable "app_name" {
   default = "tcr-staging-1"
 }
@@ -21,39 +24,41 @@ variable "app_name" {
 variable "db_user" {
   default = "postgres"
 }
+
 variable "db_name" {
   default = "production"
 }
+
 variable "db_port" {
   default = "5432"
 }
 
 locals {
   common_tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "${var.env}"
-    App = "${var.app_name}"
+    App         = "${var.app_name}"
   }
 }
 
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.access_secret}"
-  region = "${var.region}"
+  region     = "${var.region}"
 }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "${var.app_name}"
-  cidr = "172.31.0.0/16"
-  public_subnets = ["172.31.32.0/20", "172.31.64.0/20"]
+  name             = "${var.app_name}"
+  cidr             = "172.31.0.0/16"
+  public_subnets   = ["172.31.32.0/20", "172.31.64.0/20"]
   database_subnets = ["172.31.48.0/20", "172.31.80.0/20"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway   = true
   enable_dns_hostnames = true
 
-  azs = ["${var.region}a", "${var.region}b"]
+  azs  = ["${var.region}a", "${var.region}b"]
   tags = "${local.common_tags}"
 }
 
@@ -61,62 +66,63 @@ module "web_server_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
   name = "web-server-sg"
+
   # TODO make the port open within VPC behind the load balancer
   description = "Security group for web-server with HTTP ports open to public"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id      = "${module.vpc.vpc_id}"
 
-  egress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-tcp"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+  egress_rules        = ["all-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules = ["https-443-tcp", "http-80-tcp", "ssh-tcp"]
+  ingress_rules       = ["https-443-tcp", "http-80-tcp", "ssh-tcp"]
 
   tags = "${local.common_tags}"
 }
 
 module "postgres_sg" {
-  source = "terraform-aws-modules/security-group/aws//modules/postgresql"
-  name = "postgres-sg"
-  description = "Security group for postgres DB with port 5432 open within VPC"
-  vpc_id = "${module.vpc.vpc_id}"
+  source              = "terraform-aws-modules/security-group/aws//modules/postgresql"
+  name                = "postgres-sg"
+  description         = "Security group for postgres DB with port 5432 open within VPC"
+  vpc_id              = "${module.vpc.vpc_id}"
   ingress_cidr_blocks = ["${module.vpc.public_subnets_cidr_blocks[0]}"]
 }
 
 module "ec2_instance" {
-  source = "terraform-aws-modules/ec2-instance/aws"
+  source  = "terraform-aws-modules/ec2-instance/aws"
   version = "1.12.0"
 
-  name = "${var.app_name}"
+  name           = "${var.app_name}"
   instance_count = 1
 
-  ami = "${var.ami_id}"
+  ami                         = "${var.ami_id}"
   associate_public_ip_address = true
-  instance_type = "t2.small"
-  key_name = "${var.key_name}"
-  monitoring = true
-  vpc_security_group_ids = ["${module.web_server_sg.this_security_group_id}"]
-  subnet_id = "${module.vpc.public_subnets[0]}"
+  instance_type               = "t2.small"
+  key_name                    = "${var.key_name}"
+  monitoring                  = true
+  vpc_security_group_ids      = ["${module.web_server_sg.this_security_group_id}"]
+  subnet_id                   = "${module.vpc.public_subnets[0]}"
 
   tags = "${local.common_tags}"
 }
 
 module "db" {
-  source = "terraform-aws-modules/rds/aws"
-  identifier = "${var.app_name}"
-  engine = "postgres"
-  engine_version = "10.4"
-  instance_class = "db.t2.small"
+  source            = "terraform-aws-modules/rds/aws"
+  identifier        = "${var.app_name}"
+  engine            = "postgres"
+  engine_version    = "10.4"
+  instance_class    = "db.t2.small"
   allocated_storage = 1
 
   maintenance_window = "Mon:00:00-Mon:02:00"
-  backup_window = "03:00-04:00"
+  backup_window      = "03:00-04:00"
 
-  name = "${var.db_name}"
+  name     = "${var.db_name}"
   username = "${var.db_user}"
   password = "${var.db_password}"
-  port = "${var.db_port}"
+  port     = "${var.db_port}"
 
   vpc_security_group_ids = ["${module.postgres_sg.this_security_group_id}"]
-  subnet_ids = "${module.vpc.database_subnets}"
+  subnet_ids             = "${module.vpc.database_subnets}"
 
   #deletion_protection = true
 
@@ -126,4 +132,3 @@ module "db" {
 output "ec2_instance_public_dns" {
   value = "${module.ec2_instance.public_dns}"
 }
-
