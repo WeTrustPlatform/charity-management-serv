30b0d45950dfefe4a94d9f073611e8a031fd7ee6
Hoang Nguyen <hoang@wetrust.io>
2018-12-07 21:06:28 -0800

    add sanity tests

---
+++ cmd/server/main_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	. "github.com/WeTrustPlatform/charity-management-serv/cmd/server"
+	"github.com/WeTrustPlatform/charity-management-serv/seed"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	dbInstance := DB()
+	defer dbInstance.Close()
+	seed.Populate(dbInstance, "../../seed/data_test.txt", false)
+	code := m.Run()
+	os.Exit(code)
+}
+
+func execute(req *http.Request) *httptest.ResponseRecorder {
+	res := httptest.NewRecorder()
+	Router().ServeHTTP(res, req)
+	return res
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+func TestGetCharities(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/api/v0/charities", nil)
+	res := execute(req)
+	checkResponseCode(t, http.StatusOK, res.Code)
+}
+
+func TestSearchFound(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/api/v0/charities?search=Ig", nil)
+	res := execute(req)
+	checkResponseCode(t, http.StatusOK, res.Code)
+}
+
+func TestSearchNotFound(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/api/v0/charities?search=000000", nil)
+	res := execute(req)
+	checkResponseCode(t, http.StatusOK, res.Code)
+}
+
+func TestEINFound(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/api/v0/charities?ein=000587764", nil)
+	res := execute(req)
+	checkResponseCode(t, http.StatusOK, res.Code)
+}
+
+func TestEINNotFound(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/api/v0/charities?ein=000000", nil)
+	res := execute(req)
+	checkResponseCode(t, http.StatusNotFound, res.Code)
+}
+
+func TestGetCharity(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/api/v0/charities/1", nil)
+	res := execute(req)
+	checkResponseCode(t, http.StatusOK, res.Code)
+}
+
+func TestGetCharityNotFound(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/api/v0/charities/4", nil)
+	res := execute(req)
+	checkResponseCode(t, http.StatusNotFound, res.Code)
+}
