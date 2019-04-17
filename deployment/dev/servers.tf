12528576a218bfdcb3f078d997f673e87f076361
Hoang Nguyen <hoang@wetrust.io>
2019-04-17 13:55:35 -0700

    add -y to apt-get install and remove

--- deployment/dev/servers.tf
+++ deployment/dev/servers.tf
@@ -64,13 +64,13 @@ resource "null_resource" "docker" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get remove docker docker-engine docker.io containerd runc",
+      "sudo apt-get remove -y docker docker-engine docker.io containerd runc",
       "sudo apt-get update",
-      "sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common",
       "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -",
       "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable\"",
       "sudo apt-get update",
-      "sudo apt-get install docker-ce docker-ce-cli containerd.io",
+      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
     ]
   }
 }
